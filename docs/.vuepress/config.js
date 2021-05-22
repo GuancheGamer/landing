a04b0cf6eacbc47df30354c4c0abaa247d708084
Chase Hall <c@chse.xyz>
2021-05-22 13:39:02 -0500

    add favicon?
    https://github.com/vuejs/vuepress/issues/1653#issuecomment-772205524

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,8 @@ module.exports = {
     head: [
         ["meta", { name: "theme-color", content: "#3eaf7c" }],
         ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
-        ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }]
+        ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
+        ['link', { rel: "icon", type: "image/ico", sizes: "32x32", href: "favicon.ico"}],
     ],
     themeConfig: {
         repo: "plutoniumpw/docs",
